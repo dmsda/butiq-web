692e23cd469443f53aacf6a5b3817d510c3b7edc
Dimas Aditya <76651911+dmsda@users.noreply.github.com>
2023-07-29 21:28:34 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-white text-4xl font-bold hover:text-6xl duration-200 hover:text-red-200'>
+        Hi, Everyone!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
